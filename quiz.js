715eb6c286adb70b6e659806453257db7b321b46
Saviour Agbor <saviouragbor1@gmail.com>
2024-12-19 19:23:58 +0100

    Modified index & quiz.js

--- quiz.js
+++ quiz.js
@@ -3,20 +3,30 @@ function checkAnswer() {
 
   const ischecked = document.querySelector('input[name="quiz"]:checked');
 
-  if (ischecked) {
-    const userAnswer = ischecked.value;
+    if (ischecked) {
+      const userAnswer = ischecked.value;
 
-    if (userAnswer === correctAnswer) {
-      document.getElementById("feedback").textContent = "Correct! Well done.";
+      if (userAnswer === correctAnswer) {
+        let feedbackElement = document.getElementById("feedback");
+        feedbackElement.textContent = "Correct! Well done";
+        feedbackElement.style.color = "green";
+      } else {
+        let feedbackElement = document.getElementById("feedback");
+    feedbackElement.textContent = "That's incorrect. Try again!";
+    feedbackElement.style.color = "red";
+      }
     } else {
-      document.getElementById("feedback").textContent =
-        "That's incorrect. Try again!";
+      let feedbackElement = document.getElementById("feedback");
+    feedbackElement.textContent = "Please select an answer";
+    feedbackElement.style.color = "red";
     }
-  } else {
-    document.getElementById("feedback").textContent =
-      "Please select an answer.";
   }
-}
 
-const submit = document.getElementById("submit-answer");
-submit.addEventListener("click", checkAnswer);
+  const submit = document.getElementById("submit-answer");
+  submit.addEventListener("click", checkAnswer);
+
+
+
+  const para = document.getElementById('quiz-question');
+  para.textContent += " New text contents";
+  para.style.color = "green";
